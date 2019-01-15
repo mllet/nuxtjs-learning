e46e349a1ba8a3099c79fb66e35b21bb96cc51d1
mllet <1240752460@qq.com>
2019-01-15 15:50:48 +0800

    update

--- pages/user/person.vue
+++ pages/user/person.vue
@@ -1,15 +1,17 @@
 <template>
     <div>
-        <h3>嵌套路由id2</h3>
+        <h3>嵌套路由person</h3>
     </div>
 </template>
 
 <script>
     export default {
-        name: "id2"
+        name: "person"
     }
 </script>
 
-<style scoped>
-
+<style lang="less">
+h3{
+    text-align: center;
+}
 </style>
